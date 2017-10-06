4737cb5de552d614cdeb9df03601702e6e43f007
thesmashburn <dsm0015@auburn.edu>
2017-10-05 19:37:10 -0500

    Addition of new DBManager and Product and Order Classes
    Yeet

--- DataBaseManager.java
+++ DataBaseManager.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 class DataBaseManager {  
 	private static String jbdc = "com.mysql.jdbc.Driver";
 	private static String user = "smashburn";
-	private static String pass = "Comp3700";
+	private static String pass = "helloworld";
 	private static String hostDB = "jdbc:mysql://maindbinstance.cn7ucsyosm9y.us-east-2.rds.amazonaws.com/smashburnMain";
+
 
 
 	//This is a function to get a product item by ID
@@ -17,12 +18,12 @@ class DataBaseManager {
 			Statement stmt = con.createStatement();
 			String query = "SELECT * FROM product_table WHERE ID = " + _id;
 			ResultSet product = stmt.executeQuery(query);
-			con.close();
+			
 			if (product != null) {
-				return new Product(product(1), product(2), product(3), product(4), product(5), product(6));
+				return new Product(product.getString(1), product.getInt(2), product.getDouble(3), product.getString(4), product.getDouble(5), product.getDouble(6));
 			}
 		} catch (Exception e) {System.out.println(e);}
-
+		con.close();
 		return null;
 	}
 
@@ -35,13 +36,13 @@ class DataBaseManager {
 			Statement stmt = con.createStatement();
 			String query = "SELECT * FROM order_table WHERE ID = " + _id;
 			ResultSet order = stmt.executeQuery(query);
-			con.close();
+			
 			if (order != null) {
-				return new Order(order(1), order(2), order(3), order(4), order(5), order(6));
+				return new Order(order.getInt(1), order.getString(2), order.getString(3), order.getDouble(4), order.getString(5), order.getString(6));
 			}
 			
 		} catch (Exception e) {System.out.println(e);}
-
+		con.close();
 		return null;
 	}
 
@@ -54,20 +55,21 @@ class DataBaseManager {
 			Statement stmt = con.createStatement();
 			String query = "SELECT * FROM product_table";
 			ResultSet product = stmt.executeQuery(query);
-			con.close();
+			
 			
 			if (product != null) {
 				while (product.next()) {
-					result.add(new Product(product(1), product(2), product(3), product(4), product(5), product(6)));
+					result.add(new Product(product.getString(1), product.getInt(2), product.getDouble(3), product.getString(4), product.getDouble(5), product.getDouble(6)));
 				}
 			}
+			con.close();
 		} catch (Exception e) {System.out.println(e);}
 
 		return result;
 	}
 
 	//This is a function to get an order by ID
-	public static Order[] getOrders() {
+	public static ArrayList<Order> getOrders() {
 		ArrayList<Order> result = new ArrayList<Order>();
 		try {
 
@@ -76,21 +78,39 @@ class DataBaseManager {
 			Statement stmt = con.createStatement();
 			String query = "SELECT * FROM order_table";
 			ResultSet order = stmt.executeQuery(query);
-			con.close();
+			
 			if (order != null) {
 				while (order.next()) {
-					result.add(new Order(order(1), order(2), order(3), order(4), order(5), order(6)));
+					result.add(new Order(order.getInt(1), order.getString(2), order.getString(3), order.getDouble(4), order.getString(5), order.getString(6)));
 				}
 			}
-			
+			con.close();
 		} catch (Exception e) {System.out.println(e);}
 
 		return result;
 	}
 
 
+
+
+	public static Boolean updateProduct(int _id) {
+		return true;
+	}
+
+	public static Boolean addProduct(String _name, int _id, double _price, String _vendor, double _tax,  
+		double _weight) {
+		return true;
+
+
+	}
+
 	public static void main(String args[]){  
 		System.out.println("booty");
+		ArrayList<Product> test = DataBaseManager.getProducts();
+		for (Product i : test) {
+			System.out.println(i.getInfo());
+		}
+
 
 	}
 
