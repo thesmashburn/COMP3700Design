62c6e2b64500b8501b778e64f3d016128a9897c5
thesmashburn <dsm0015@auburn.edu>
2017-10-19 20:35:46 -0500

    SrcFiles for NetBeans

---
+++ Market/UserSystem.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.ArrayList; 
+
+public class UserSystem {
+	private  ArrayList<Product> currentProductList;
+	private ArrayList<Order> currentOrderList;
+
+	public UserSystem() {
+		currentProductList = DataBaseManager.getProducts();
+		currentOrderList = DataBaseManager.getOrders();
+	}
+
+	public ArrayList<Product> getProductList() {
+		return currentProductList;
+	}
+
+	public ArrayList<Order> getOrderList() {
+		return currentOrderList;
+	}
+
+	public void updateProductList() {
+		currentProductList = DataBaseManager.getProducts();
+	}
+
+	public void updateOrderList() {
+		currentOrderList = DataBaseManager.getOrders();
+	}
+
+}
